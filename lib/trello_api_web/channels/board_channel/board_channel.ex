88e71f9cbea1bcd817d9425641d81115334bed31
Nicolas Ferraro <nferraro1996@gmail.com>
2019-10-17 16:17:09 -0300

    Add missing board_channel

---
+++ lib/trello_api_web/channels/board_channel/board_channel.ex
@@ -0,0 +1,18 @@
+defmodule TrelloApiWeb.BoardChannel do
+  use Phoenix.Channel
+
+  alias TrelloApi.BoardList
+
+  @impl true
+  def join("board:" <> board_id, _params, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("card_created", %{"list_id" => list_id}, socket) do
+    board_list = BoardList.get(list_id)
+
+    broadcast!(socket, "user_created_card", %{list_id: list_id, cards: board_list.cards})
+
+    {:noreply, socket}
+  end
+end
